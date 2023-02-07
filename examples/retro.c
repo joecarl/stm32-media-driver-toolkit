02b5c62edf41b9e73a2a8fbc4961daa580f92c70
José Carlos HR <joecarlhr@gmail.com>
2023-02-07 12:55:51 +0100

    add blinking sprite and minor changes

--- examples/retro.c
+++ examples/retro.c
@@ -20,6 +20,7 @@
 #include "libs/clkinfo.h"
 
 #include "audio_samples.h"
+#include "sprites.h"
 
 
 void DrawStripedCircle(BITMAP* bmp, float x, float y, float radius, float strip_offset, uint8_t color) {
@@ -44,8 +45,6 @@ void DrawStripedCircle(BITMAP* bmp, float x, float y, float radius, float strip_
 
 void retro_demo(void) {
 
-	char str[50];
-	
 	GRAPHICS_InitTypeDef graphicsCfg = {
 		.useHardwareAcceleration = true,
 		.useSDRAM = false
@@ -54,6 +53,8 @@ void retro_demo(void) {
 	VGA_Init_Signal(VGA_320x200);
 	AUDIO_Init();
 
+	//char str[50];
+	uint16_t tick = 0;
 	const float vpx = 150;
 	const float vpy = 80;
 	const float d = 9.0;
@@ -63,18 +64,19 @@ void retro_demo(void) {
 
 	BITMAP ctx_bmp;
 
+
 	while (1) {
 		
 		GRAPHICS_GetBitmapFromContext(&ctx_bmp, &main_ctx);
 
 		if (!AUDIO_IsPlaying()) {
-			//PlayAquaticAmbience();
+			PlayStrangerThings();
 		}
 		
 		ClearBitmap(0x00);
 		
 		//sprintf(str, "fps: %u", VGA_GetFPS());
-		//DrawText(str, 20, 160, 0xFF);
+		//DrawText(str, 20, 160, 0xC7);
 	
 		const float y_far = 100;
 		const float y_near = 200;
@@ -87,14 +89,14 @@ void retro_demo(void) {
 			const float x_far = i * 20 + sin(6.28 * x_offset) * 50.0;
 			const float x_near = x_far + (x_far - vpx) * (y_near - vpy) / (y_far - vpy);
 		
-			DrawLine(x_near, y_near, x_far, y_far, 0xFF);
+			DrawLine(x_near, y_near, x_far, y_far, 0xC7);
 
 		}
 
 
 		//Draw far line
 
-		DrawLine(0, y_far, 320, y_far, 0xFF);
+		DrawLine(0, y_far, 320, y_far, 0xC7);
 
 		
 		//Calc horizontal lines projections
@@ -107,13 +109,19 @@ void retro_demo(void) {
 			const float y = 200 - ((200 - vpy) / (z + d)) * z;
 			if (y < y_far) continue;
 		
-			DrawLine(0, y, 320, y, 0xFF);
+			DrawLine(0, y, 320, y, 0xC7);
 
 		}
 
-		DrawStripedCircle(&ctx_bmp, 150, 80, 40, 6.28 * z_offset, 0xC7);
-
 		
+		DrawStripedCircle(&ctx_bmp, 150, 80, 40, 6.28 * z_offset, tick % 256);// 0xC7);
+		
+		const uint16_t m_tick = tick % 500;
+		if (m_tick >= 465 && m_tick < 469 || m_tick >= 474 && m_tick < 494 || m_tick >= 498 && m_tick < 500) {
+			DrawCreature(110, 50);
+		} else {
+		}
+
 		//Increment movement variables
 
 		x_offset += 0.002;
@@ -128,6 +136,7 @@ void retro_demo(void) {
 			z_offset = 0;
 		}
 
+		tick++;
 
 		VGA_WaitForVSync();
 		VGA_SwapBuffers();
