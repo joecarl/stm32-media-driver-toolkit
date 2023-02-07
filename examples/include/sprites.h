2496897e8a0c8bcc985cc31482f8cb15a975a8da
José Carlos HR <joecarlhr@gmail.com>
2023-02-07 12:54:45 +0100

    add creature sprite

--- examples/include/sprites.h
+++ examples/include/sprites.h
@@ -7,5 +7,6 @@ void DrawMario(BITMAP* bmp, int x, int y, int spr);
 void DrawSuelo(BITMAP* bmp, int x, int y);
 void DrawTuberia(BITMAP* bmp, int x, int y, int height);
 void DrawLogo(int x, int y);
+void DrawCreature(int x, int y);
 
 #endif
