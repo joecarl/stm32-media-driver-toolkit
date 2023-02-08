14547f81427809911a59590f1a80aa5f3cfda868
José Carlos HR <joecarlhr@gmail.com>
2023-02-08 01:21:32 +0100

    add more notes

--- include/drivers/audio_driver.h
+++ include/drivers/audio_driver.h
@@ -3,48 +3,66 @@
 
 #include <stdint.h>
 
-#define C4	261.63
+#define C3  130.81
+#define Cs3 138.59
+#define Db3 138.59
+#define D3  146.83
+#define Ds3 155.56
+#define Eb3 155.56
+#define E3  164.81
+#define F3  174.61
+#define Fs3 185.00
+#define Gb3 185.00
+#define G3  196.00
+#define Gs3 207.65
+#define Ab3 207.65
+#define A3  220.00
+#define As3 233.08
+#define Bb3 233.08
+#define B3  246.94
+
+#define C4  261.63
 #define Cs4 277.18
 #define Db4 277.18
-#define D4	293.66
+#define D4  293.66
 #define Ds4 311.13
 #define Eb4 311.13
-#define E4	329.63
-#define F4	349.23
-#define Fs4	369.99
+#define E4  329.63
+#define F4  349.23
+#define Fs4 369.99
 #define Gb4 369.99
-#define G4	392.00
-#define Gs4	415.30
-#define Ab4	415.30
-#define A4	440.00
-#define As4	466.16
-#define Bb4	466.16
-#define B4	493.88
+#define G4  392.00
+#define Gs4 415.30
+#define Ab4 415.30
+#define A4  440.00
+#define As4 466.16
+#define Bb4 466.16
+#define B4  493.88
 
-#define C5	523.25
+#define C5  523.25
 #define Cs5 554.37
 #define Db5 554.37
-#define D5	587.33
-#define Ds5	622.25
+#define D5  587.33
+#define Ds5 622.25
 #define Eb5 622.25
-#define E5	659.25
-#define F5	698.46
-#define Fs5	739.99
+#define E5  659.25
+#define F5  698.46
+#define Fs5 739.99
 #define Gb5 739.99
-#define G5	783.99
-#define Gs5	830.61
+#define G5  783.99
+#define Gs5 830.61
 #define Ab5 830.61
-#define A5	880.00
-#define As5	932.33
-#define Bb5	932.33
-#define B5	987.77
+#define A5  880.00
+#define As5 932.33
+#define Bb5 932.33
+#define B5  987.77
 
-#define C6	1046.50
-#define Cs6	1108.73
-#define Db6	1108.73
-#define D6	1174.66
-#define Ds6	1244.51
-#define Eb6	1244.51
+#define C6  1046.50
+#define Cs6 1108.73
+#define Db6 1108.73
+#define D6  1174.66
+#define Ds6 1244.51
+#define Eb6 1244.51
 
 
 typedef struct {
@@ -53,8 +71,11 @@ typedef struct {
 } Nota;
 
 void AUDIO_Init();
+
 void AUDIO_Play();
+
 void AUDIO_AddNote(uint16_t frec, uint16_t time);
+
 uint8_t AUDIO_IsPlaying();
 
 #endif
