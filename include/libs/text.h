0889ec19e2432ba78871f92beeac400929484c78
José Carlos HR <joecarlhr@gmail.com>
2023-02-16 01:15:07 +0100

    add text spacing definitions

--- include/libs/text.h
+++ include/libs/text.h
@@ -4,6 +4,9 @@
 #include "stm32f4xx.h"
 #include "libs/graphics.h"
 
+#define TEXT_CHAR_WIDTH 6
+#define TEXT_LINE_HEIGHT 10
+
 
 void GRAPHICS_DrawText(BITMAP* bmp, const char* text, int x, int y, uint8_t color);
 
