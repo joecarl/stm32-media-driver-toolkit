efe2d7bfeb4da3ea2fc7baaf55c73db02d4bb08d
José Carlos HR <joecarlhr@gmail.com>
2023-02-18 20:02:42 +0100

    add report macros

--- include/libs/text.h
+++ include/libs/text.h
@@ -6,6 +6,18 @@
 #define TEXT_CHAR_WIDTH 6
 #define TEXT_LINE_HEIGHT 10
 
+#define REPORT(t, x, y, c) {\
+	char str[100];\
+	sprintf(str, #t": 0x%X", (t));\
+	DrawText(str, (x), (y), (c));\
+} 
+
+#define REPORT_L(l, t, x, y, c) {\
+	char str[100];\
+	sprintf(str, l": 0x%X", (t));\
+	DrawText(str, (x), (y), (c));\
+} 
+
 
 void GRAPHICS_DrawText(BITMAP* bmp, const char* text, int x, int y, uint8_t color);
 
