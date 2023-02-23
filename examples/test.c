37cbb7907c6f8f03f6bb75c99c7fffa49ca5e274
José Carlos HR <joecarlhr@gmail.com>
2023-02-23 21:02:20 +0100

    improve modulation

--- examples/test.c
+++ examples/test.c
@@ -28,11 +28,27 @@ uint8_t move = 1;
 float speed = 0.25;
 
 
+void DrawColorPalletes(BITMAP* bmp, int x, int y) {
+
+	for (uint8_t ck = 0; ck < 4; ck++)
+		for (uint8_t ci = 0; ci < 8; ci++)
+			for (uint8_t cj = 0; cj < 8; cj++)
+				for (uint8_t i = 0; i < 4; i++)
+					for (uint8_t j = 0; j < 4; j++)
+						GRAPHICS_PutPixel(
+							bmp,
+							x + ci * 4 + i + 40 * ck, 
+							y + cj * 4 + j, 
+							ci + (cj << 3) + (ck << 6)
+						);
+}
+
+
 void test_all(void) {
 
 	float time = 0, angle = 0;
 	uint8_t time_direction = 1;
-	char str[50];
+	char str[128];
 	
 	GRAPHICS_InitTypeDef graphicsCfg = {
 		.useHardwareAcceleration = true,
@@ -56,31 +72,22 @@ void test_all(void) {
 		
 		ClearBitmap(0x00);
 		
-		sprintf(str, "fps: %u", GetFPS());
-		DrawText(str, 20, 160, 0xFF);
-		
 		//Pintamos las paletas de colores
-		for (uint8_t ck = 0; ck < 4; ck++)
-			for (uint8_t ci = 0; ci < 8; ci++)
-				for (uint8_t cj = 0; cj < 8; cj++)
-					for (uint8_t i = 0; i < 4; i++)
-						for (uint8_t j = 0; j < 4; j++)
-							GRAPHICS_PutPixel(
-								&ctx_bmp,
-								ci * 4 + i + 10 + 40 * ck, 
-								cj * 4 + j + 30, 
-								ci + (cj << 3) + (ck << 6)
-							);
+		DrawText("Paletas de colores", 10, 10, 0xFF);
+		DrawColorPalletes(&ctx_bmp, 10, 30);
+
 		
+		sprintf(str, "FPS: %u", GetFPS());
+		DrawText(str, 10, 80, 0x1F);
+
 		//for (uint32_t it = 0; it < 1000; it++)
 		//	DrawLine(it/10, it/20, it%100, it%200+20, 0xF4);	
 
-		DrawText("Paletas de colores", 10, 10, 0xFF);
 		DrawText("abcdefghijklmnñopqrstuvwxyz", 10, 100, 0xFF);
 		DrawText("ABCDEFGHIJKLMNÑOPQRSTUVWXYZ", 10, 110, 0xFF);
 		DrawText("0123456789", 10, 120, 0xFF);
 
-		sprintf(str, "APB1 Timers freq: %d MHz", (int32_t) GetAPB1TimersMHz());
+		sprintf(str, "APB1 Timers freq: %d MHz", (int) GetAPB1TimersMHz());
 		DrawText(str, 10, 130, 0xFA);
 
 		Draw3DPyramid(170, 270, 3, angle, 30, 50, 1);
@@ -90,7 +97,7 @@ void test_all(void) {
 		DrawText("Gráficos en 3 dimensiones...", -200 + time, 180, 0xA5);
 
 		float ry = 20 + 0.3 * time;
-		DrawCircle(time - 50, ry, 20 + time / 20, 2.0, 0xF6);
+		DrawCircle(time - 50, ry, 20 + time / 20, 1.0, 0xF6);
 		//sprintf(str, "move: %d", move);
 		//DrawText(str, 20, 450, 0xFF);
 		if (move)
